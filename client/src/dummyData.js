2d5479895bf5ed4b96471edcc21c9e6f58d450e5
Isha <ishac9879@gmail.com>
2022-01-03 01:16:12 -0500

    first commit

---
+++ client/src/dummyData.js
@@ -0,0 +1,29 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "person/1.jpeg",
+    username: "Sara Sha",
+  },
+  {
+    id: 2,
+    profilePicture: "person/2.jpeg",
+    username: "Jailyn Kay",
+  },
+  {
+    id: 3,
+    profilePicture: "person/3.jpeg",
+    username: "Arsan Kim",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "post/1.jpeg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+];
---
+++ client/src/pages/messenger/Messenger.jsx
@@ -0,0 +1,9 @@
+import "./messenger.css"
+
+export default function Messenger() {
+    return (
+        <div className="messenger">
+            hello
+        </div>
+    )
+}
